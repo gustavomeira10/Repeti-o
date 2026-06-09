adee50c923b8d19b98ae599962a816de97588585
gustavomeira10 <gustavo.meira.oliveira22@gmail.com>
2026-06-09 12:32:13 -0300

    CodigoFibonacci.java

--- CodigoFibonacci.java
+++ CodigoFibonacci.java
@@ -1,7 +1,16 @@
 package Repeticao;
-
-public class CodigoFibonacci {
 public static void main(String[] args) {
-	for (int i = 0; )
-}
+        int numero1 = 0;
+        int numero2 = 1;
+        
+        System.out.println("Os 10 primeiros números de Fibonacci são: ");
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.print(n1 + " ");
+            
+            int proximo = n1 + n2;
+            n1 = n2;
+            n2 = proximo;
+        }
+    }
 }
